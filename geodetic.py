379d14991af6f83ffff7ce38cecc9b56a439e7fd
Utku Can <utkucancanatan@gmail.com>
2022-05-27 05:14:37 +0300

    UPDATE

--- geodetic.py
+++ geodetic.py
@@ -13,9 +13,25 @@ f1=[299.1528,297,298.257222101]
 e=[0.00667437,0.00672267,0.0066943800]
 e2=[0.00671922,0.00676817,0.0067394968]
 n=[0.00167418,0.00168634,0.0016792204]
-
+alfa = [366742.519778,6367654.500058,6367449.14571]
+beta = [15988.639219,16107.034679,16038.508742]
+fi = [16.729955,16.976211,16.832613]
+gama = [0.021785,0.022266,0.021984]
+el = [0.000031,0.000032,0.000031]
+beta1 = [0.002511273350,0.002529506915,0.002518826597]
+fi1 = [0.000003678786,0.000003732401,0.000003700949]
+gama1 = [0.000000007381,0.000000007543,0.000000007448]
+el1 = [0.000000000017,0.000000000017,0.000000000017]
 class Bassel:
     def printdata():
+        """
+        ALL GEODESIC DATA PRINT
+
+        Returns
+        -------
+        None
+
+        """
         na = a[1]
         nb = b[1]
         nc = c[1]
@@ -23,13 +39,24 @@ class Bassel:
         ne = e[1]
         ne2 = e2[1]
         nn = n[1]
-        print("a"+str(na))
-        print("b"+str(nb))
-        print("c"+str(nc))
-        print("1/f"+str(nf1))
-        print("e"+str(ne))
-        print("e'^2"+str(ne2))
-        print("n"+str(nn))
+        nalfa = alfa[1]
+        nbeta = beta[1]
+        nfi = fi[1]
+        ngama = gama[1]
+        nel = el[1]
+        nbeta1 = beta1[1]
+        nfi1 = fi1[1]
+        ngama1 = gama1[1]
+        nel1 = el1[1]
+        print("a ="+str(na))
+        print("b ="+str(nb))
+        print("c ="+str(nc))
+        print("1/f ="+str(nf1))
+        print("e ="+str(ne))
+        print("e'^2 ="+str(ne2))
+        print("n ="+str(nn))
+        print("α ="+str(nalfa))
+        print("β ="+str(nbeta))
     def Ncalc(phi):
         """
         
