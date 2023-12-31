c356c4f021539c233f851b5f93954d1bc98a1bef
Utku Can <utkucancanatan@gmail.com>
2023-12-31 12:38:51 +0300

    matrix

--- matrixcalc.py
+++ matrixcalc.py
@@ -7,6 +7,7 @@ Created on Sat Dec 30 21:46:49 2023
 
 import math
 from . import core
+import numpy as np
 
 class basic():
     def __init__(self):
@@ -47,7 +48,93 @@ class basic():
             i = i+1
         result = 0
         return result
-    
+    def GetMatrixFromFile(file):
+        """
+        
+
+        Parameters
+        ----------
+        file : Txt File Folder
+        Example txt file:
+            
+            0,0,0,0,0,0,0,0,0,0
+            0,0,0,0,0,0,0,0,0,0
+            0,0,0,0,0,0,0,0,0,0
+            0,0,0,0,0,0,0,0,0,0
+            0,0,0,0,0,0,0,0,0,0
+            0,0,0,0,0,0,0,0,0,0
+            0,0,2,1,0,2,0,0,0,0
+            0,0,2,1,1,2,2,0,0,1
+            0,0,1,2,2,1,1,0,0,2
+            1,0,1,1,1,2,1,0,2,1
+
+        Returns
+        -------
+        matris : TYPE
+            Matris
+
+        """
+        with open(file, 'r') as f:
+            matris = [[int(num) for num in line.split(',')] for line in f]
+        return matris
+    def Matrixadd(matris1,matris2):
+        """
+        
+
+        Parameters
+        ----------
+        matris1 : Matrix
+            
+        matris2 : Matrix
+            DESCRIPTION.
+
+        Returns
+        -------
+        result : TYPE
+            DESCRIPTION.
+
+        """
+        sh1 = np.array(matris1).shape
+        sh2 = np.array(matris2).shape
+        row = sh1[0]
+        result = [[0 for _ in range(row)] for _ in range(row)]
+        if sh1 == sh2 :
+            i,j = 0
+            for i in range(sh1[0]):
+                for j in range(sh1[0]):
+                    result[i][j] = matris1[i][j] + matris2[i][j]
+           # print(result)
+        
+        return result
+    def MatrixSubtraction(matris1,matris2):
+        """
+        
+
+        Parameters
+        ----------
+        matris1 : Matrix
+            
+        matris2 : Matrix
+            DESCRIPTION.
+
+        Returns
+        -------
+        result : TYPE
+            DESCRIPTION.
+
+        """
+        sh1 = np.array(matris1).shape
+        sh2 = np.array(matris2).shape
+        row = sh1[0]
+        result = [[0 for _ in range(row)] for _ in range(row)]
+        if sh1 == sh2 :
+            i,j = 0
+            for i in range(sh1[0]):
+                for j in range(sh1[0]):
+                    result[i][j] = matris1[i][j] - matris2[i][j]
+            
+        
+        return result
 class multi():
     def __init__(self):
         return self
